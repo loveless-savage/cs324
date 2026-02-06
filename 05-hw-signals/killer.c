7345b430715287ef21a20d4eb407de1d6a6aae4f
loveless-savage <titansofalgenia@gmail.com>
2026-02-05 22:58:16 -0700

    hw5 complete

--- 05-hw-signals/killer.c
+++ 05-hw-signals/killer.c
@@ -26,26 +26,121 @@ int main(int argc, char *argv[]) {
 	// this one and, more importantly, to the child.
 	sigaction(SIGINT, &sigact, NULL);
 
+	//int h[10] = {-1,SIGHUP,SIGQUIT,SIGTERM,30,10,16,31,12,SIGCHLD};
+	/* Signal-to-handler dictionary:
+	 * [handler#] = [SIG#]
+	 * 1 = SIGHUP | SIGINT
+	 * 2 = SIGQUIT
+	 * 3 = SIGTERM
+	 * 4 = 30
+	 * 5 = 10
+	 * 6 = 16
+	 * 7 = 31
+	 * 8 = 12
+	 * 9 = SIGCHLD
+	 *
+	 * cool vim macro I used:
+	 * while writing this I didn't want to keep track of the mapping between
+	 * signal codes and handlers, so I defined int h[10] above
+	 * and used h[#] values instead of the codes in my kill() statements.
+	 * Once I was finished I wanted to replace them all at once, so I wrote
+	 * this vim macro:
+	 *     :let @j=line(".")29gg0:normal jf,€ý5l"kye:%s/h\[j\]/kj
+	 * Then I positioned myself at line 1 and ran it 9 times.
+	 * The macro copies down my current line as the handler index,
+	 * goes to the declaration of h[] and yanks the appropriate signal code,
+	 * and does a document-wide search-and-replace for that code.
+	 * Finally it ends one line lower, so I can run it immediately again.
+	 * It was a fun puzzle to figure out how to do this, I thought Dr. Deccio
+	 * or any TA who uses vim might get a kick out of that.
+	 */
+
 	switch (scenario[0]) {
 	case '0':
+		kill(pid, SIGHUP);
 		break;
 	case '1':
+		kill(pid, 12);
+		sleep(1);
+		kill(pid, SIGTERM);
 		break;
 	case '2':
+		kill(pid, SIGHUP);
+		sleep(5);
+		kill(pid, 12);
+		sleep(1);
+		kill(pid, SIGTERM);
 		break;
 	case '3':
+		kill(pid, SIGHUP);
+		sleep(1);
+		kill(pid, SIGHUP);
+		sleep(8);
+		kill(pid, 12);
+		sleep(1);
+		kill(pid, SIGTERM);
 		break;
 	case '4':
+		kill(pid, SIGHUP);
+		sleep(1);
+		kill(pid, SIGINT);
+		sleep(8);
+		kill(pid, 12);
+		sleep(1);
+		kill(pid, SIGTERM);
 		break;
 	case '5':
+		kill(pid, 12);
+		sleep(1);
+		kill(pid, SIGHUP);
+		sleep(1);
+		kill(pid, SIGTERM);
 		break;
 	case '6':
+		kill(pid, SIGHUP);
+		sleep(5);
+		kill(pid, 10);
+		sleep(1);
+		kill(pid, 12);
+		sleep(1);
+		kill(pid, SIGTERM);
 		break;
 	case '7':
+		kill(pid, SIGHUP);
+		sleep(5);
+		kill(pid, 10);
+		sleep(1);
+		kill(pid, 16);
+		sleep(1);
+		kill(pid, 12);
+		sleep(1);
+		kill(pid, SIGTERM);
 		break;
 	case '8':
+		kill(pid, SIGHUP);
+		sleep(1);
+		kill(pid, 31);
+		sleep(5);
+		kill(pid, 10);
+		sleep(1);
+		kill(pid, 30);
+		sleep(1);
+		kill(pid, SIGTERM);
+		sleep(1);
+		kill(pid, 12);
+		sleep(1);
+		kill(pid, SIGTERM);
 		break;
 	case '9': 
+		kill(pid, 31);
+		sleep(1);
+		kill(pid, SIGQUIT);
+		sleep(5);
+		kill(pid, 31);
+		sleep(5);
+		kill(pid, 12);
+		sleep(1);
+		kill(pid, SIGTERM);
 		break;
 
 	}

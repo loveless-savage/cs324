1cde16145518b2fdb9d0e0ec121784bf62963757
Andrew Kimball Jones <akj53@carolina.cs.byu.edu>
2026-03-05 20:29:22 -0700

    lab8 checkpoint 2

--- 08-lab-socket-hunt/treasure_hunter.c
+++ 08-lab-socket-hunt/treasure_hunter.c
@@ -1,16 +1,52 @@
 // Replace PUT_USERID_HERE with your actual BYU CS user id, which you can find
 // by running `id -u` on a CS lab machine.
-#define USERID PUT_USERID_HERE
+#define USERID 1823703346
+#define BUFSIZE 8
 
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
 #include "sockhelper.h"
 
 int verbose = 0;
 
+void prep_msg(unsigned char* buf, int level, int seed);
 void print_bytes(unsigned char *bytes, int byteslen);
 
 int main(int argc, char *argv[]) {
+	// parse arguments
+	if (argc != 5) {
+		fprintf(stderr,"Usage: ./treasure_hunter [server_domain_name] [port#] [level#] [seed#]\n");
+		exit(1);
+	}
+	char *server_domain_name = argv[1];
+	char *port = argv[2];
+	int level = atoi(argv[3]);
+	if (level<0 || level>4) {
+		fprintf(stderr,"argument #3 must be a level number between 0 and 4\n");
+		exit(1);
+	}
+	int seed = atoi(argv[4]);
+
+	// load arguments into message request
+	unsigned char buf[BUFSIZE];
+	prep_msg(buf,level,seed);
+	printf("memory copied\n");
+	fflush(stdout);
+	print_bytes(buf,BUFSIZE);
+}
+
+void prep_msg(unsigned char* buf, int level, int seed) {
+	bzero(buf,BUFSIZE);
+
+	buf[1] = level;
+
+	unsigned int userid = (unsigned int)htonl(USERID);
+	memcpy(&buf[2],&userid,sizeof(unsigned int));
+
+	unsigned short x = htons(seed);
+	memcpy(&buf[6],&x,sizeof(unsigned short));
 }
 
 void print_bytes(unsigned char *bytes, int byteslen) {

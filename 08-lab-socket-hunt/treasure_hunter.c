46f5a00f5e25484c0a94efad58e6c64c3bf52615
Andrew Kimball Jones <akj53@carolina.cs.byu.edu>
2026-03-05 23:05:56 -0700

    lab8 checkpoint 6

--- 08-lab-socket-hunt/treasure_hunter.c
+++ 08-lab-socket-hunt/treasure_hunter.c
@@ -3,9 +3,11 @@
 #define USERID 1823703346
 #define BUFSIZE 8
 #define MAX_RECVSIZE 256
+#define MAX_TOT_RECVSIZE 1024
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
@@ -13,9 +15,14 @@
 #include <netdb.h>
 #include "sockhelper.h"
 
+int sfd = -1;
+struct sockaddr *remote_addr = NULL;
+socklen_t addr_len = sizeof(struct sockaddr_storage);
 int verbose = 0;
 
 void prep_msg(unsigned char* buf, int level, int seed);
+void request(unsigned char *buf, size_t send_size, unsigned char *bufrecv);
+void parse_response(unsigned char *bufrecv, unsigned char chunk_len, unsigned char *treasure, unsigned char *op, unsigned short *op_param, unsigned int *nonce);
 void print_bytes(unsigned char *bytes, int byteslen);
 
 int main(int argc, char *argv[]) {
@@ -51,14 +58,16 @@ int main(int argc, char *argv[]) {
 	}
 	// parse query response
 	struct sockaddr_storage remote_addr_ss;
-	struct sockaddr *remote_addr = (struct sockaddr *)&remote_addr_ss;
+	remote_addr = (struct sockaddr *)&remote_addr_ss;
 	memcpy(remote_addr, result->ai_addr, sizeof(struct sockaddr_storage));
 	char remote_ip[INET6_ADDRSTRLEN];
 	unsigned short remote_port;
 	parse_sockaddr(remote_addr, remote_ip, &remote_port);
-	fprintf(stderr, "Connecting to %s:%d\n", remote_ip, remote_port);
+	if (verbose) {
+		fprintf(stderr, "Connecting to %s:%d\n", remote_ip, remote_port);
+	}
 	// initialize socket
-	int sfd = socket(AF_INET, SOCK_DGRAM, 0);
+	sfd = socket(AF_INET, SOCK_DGRAM, 0);
 	if (sfd<0) {
 		perror("socket");
 		exit(EXIT_FAILURE);
@@ -66,50 +75,60 @@ int main(int argc, char *argv[]) {
 	// capture local address info
 	struct sockaddr_storage local_addr_ss;
 	struct sockaddr *local_addr = (struct sockaddr *)&local_addr_ss;
-	socklen_t addr_len = sizeof(struct sockaddr_storage);
 	s = getsockname(sfd, local_addr, &addr_len);
 	char local_ip[INET6_ADDRSTRLEN];
 	unsigned short local_port;
 	parse_sockaddr(local_addr, local_ip, &local_port);
-	fprintf(stderr, "Local address %s:%d\n", local_ip, local_port);
 
 	// send message and collect response
-	ssize_t nwritten = sendto(sfd, buf, BUFSIZE, 0, remote_addr, addr_len);
-	if (nwritten<0) {
-		perror("sendto");
-		exit(EXIT_FAILURE);
-	}
 	unsigned char bufrecv[MAX_RECVSIZE];
-	ssize_t nread = recvfrom(sfd, bufrecv, MAX_RECVSIZE, 0, remote_addr, &addr_len);
-	if (nread<0) {
-		perror("recvfrom");
-		exit(EXIT_FAILURE);
-	}
-	printf("Response is %d bytes:",(int)nread);
-	print_bytes(bufrecv,nread);
-
+	request(buf,BUFSIZE,bufrecv);
 	// parse response
-	unsigned char chunk_len = bufrecv[0];
+	unsigned char chunk_len = 0;
+	unsigned char full_treasure[MAX_TOT_RECVSIZE];
+	unsigned char *treasure = full_treasure;
+	unsigned char op;
+	unsigned short op_param;
+	unsigned int nonce, req;
+	chunk_len = bufrecv[0];
 	if (chunk_len==0) {
 		// TODO: treasure hunt over
 	} else if (chunk_len>127) {
 		// TODO: server error
 	}
-	printf("chunk length = %d\t",chunk_len);
-	unsigned char treasure[chunk_len+1];
-	memcpy(treasure,&bufrecv[1],chunk_len);
-	treasure[chunk_len] = '\0';
-	printf("treasure = %s\t",treasure);
-	unsigned char op = bufrecv[chunk_len+1];
-	printf("op = 0x%x\t",op);
-	unsigned short param_ns;
-	memcpy(&param_ns,&bufrecv[chunk_len+2],sizeof(unsigned short));
-	unsigned short op_param = ntohs(param_ns);
-	printf("op_param = 0x%x\t",op_param);
-	unsigned int nonce_ns;
-	memcpy(&nonce_ns,&bufrecv[chunk_len+4],sizeof(unsigned int));
-	unsigned int nonce = (unsigned int)ntohl(nonce_ns);
-	printf("nonce = 0x%x\n",nonce);
+	parse_response(bufrecv,chunk_len,treasure,&op,&op_param,&nonce);
+	if (verbose) {
+		printf("chunk length = %d\ttreasure = %s\top = 0x%x\top_param = 0x%x\tnonce = 0x%x\n",
+				chunk_len,treasure,op,op_param,nonce);
+	}
+	treasure += chunk_len;
+
+	// enter request + response loop
+	while (1) {
+		// follow-up request
+		req = (unsigned int)htonl(nonce+1);
+		if (verbose) {
+			print_bytes((unsigned char *)&req,sizeof(unsigned int));
+		}
+		request((unsigned char *)&req, sizeof(unsigned int), bufrecv);
+
+		chunk_len = bufrecv[0];
+		if (chunk_len==0) {
+			break;
+		} else if (chunk_len>127) {
+			// TODO: server error
+		}
+		parse_response(bufrecv,chunk_len,treasure,&op,&op_param,&nonce);
+		if (verbose) {
+			printf("chunk length = %d\ttreasure = %s\top = 0x%x\top_param = 0x%x\tnonce = 0x%x\n",
+					chunk_len,treasure,op,op_param,nonce);
+		}
+		treasure += chunk_len;
+	}
+	*treasure = '\0';
+	printf("%s\n",full_treasure);
+
+	close(sfd);
 }
 
 
@@ -123,6 +142,38 @@ void prep_msg(unsigned char* buf, int level, int seed) {
 
 	unsigned short seed_ns = htons(seed);
 	memcpy(&buf[6],&seed_ns,sizeof(unsigned short));
+}
+
+void request(unsigned char *buf, size_t send_size, unsigned char *bufrecv) {
+	ssize_t nwritten = sendto(sfd, buf, send_size, 0, remote_addr, addr_len);
+	if (nwritten<0) {
+		perror("sendto");
+		exit(EXIT_FAILURE);
+	}
+	ssize_t nread = recvfrom(sfd, bufrecv, MAX_RECVSIZE, 0, remote_addr, &addr_len);
+	if (nread<0) {
+		perror("recvfrom");
+		exit(EXIT_FAILURE);
+	}
+	if (verbose) {
+		printf("Response is %d bytes:",(int)nread);
+		print_bytes(bufrecv,nread);
+	}
+}
+
+void parse_response(unsigned char *bufrecv, unsigned char chunk_len, unsigned char *treasure, unsigned char *op, unsigned short *op_param, unsigned int *nonce) {
+	memcpy(treasure,&bufrecv[1],chunk_len);
+	//treasure[chunk_len] = '\0';
+
+	*op = bufrecv[chunk_len+1];
+
+	unsigned short param_ns;
+	memcpy(&param_ns,&bufrecv[chunk_len+2],sizeof(unsigned short));
+	*op_param = ntohs(param_ns);
+
+	unsigned int nonce_ns;
+	memcpy(&nonce_ns,&bufrecv[chunk_len+4],sizeof(unsigned int));
+	*nonce = (unsigned int)ntohl(nonce_ns);
 }
 
 void print_bytes(unsigned char *bytes, int byteslen) {

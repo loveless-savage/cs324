9f022e5cf54c825bca08872db8c9abc175f1056c
Andrew Kimball Jones <akj53@rhodeisland.cs.byu.edu>
2026-03-06 17:43:58 -0700

    lab8 checkpoint 8

--- 08-lab-socket-hunt/treasure_hunter.c
+++ 08-lab-socket-hunt/treasure_hunter.c
@@ -15,37 +15,38 @@
 #include <netdb.h>
 #include "sockhelper.h"
 
+int verbose = 0;
 int sfd = -1;
 int addr_fam = AF_INET;
 struct sockaddr *remote_addr = NULL, *local_addr = NULL;
 char remote_ip[INET6_ADDRSTRLEN], local_ip[INET6_ADDRSTRLEN];
 unsigned short remote_port, local_port;
 socklen_t addr_len = sizeof(struct sockaddr_storage);
-int verbose = 0;
 
-void prep_msg(unsigned char* buf, int level, int seed);
+void prep_initial_msg(unsigned char* buf, int level, int seed);
 void request(unsigned char *buf, size_t send_size, unsigned char *bufrecv);
 void parse_response(unsigned char *bufrecv, unsigned char chunk_len, unsigned char *treasure, unsigned char *op, unsigned short *op_param, unsigned int *nonce);
+unsigned int op_action(unsigned char op, unsigned short op_param, unsigned int nonce);
 void print_bytes(unsigned char *bytes, int byteslen);
 
 int main(int argc, char *argv[]) {
 	// parse arguments
 	if (argc != 5) {
-		fprintf(stderr,"Usage: ./treasure_hunter [server_domain_name] [port#] [level#] [seed#]\n");
+		printf("Usage: ./treasure_hunter [server_domain_name] [port#] [level#] [seed#]\n");
 		exit(EXIT_FAILURE);
 	}
 	char *server_domain_name = argv[1];
 	char *port_in = argv[2];
 	int level = atoi(argv[3]);
 	if (level<0 || level>4) {
-		fprintf(stderr,"argument #3 must be a level number between 0 and 4\n");
+		printf("argument #3 must be a level number between 0 and 4\n");
 		exit(EXIT_FAILURE);
 	}
 	int seed = atoi(argv[4]);
 
 	// load arguments into message request
 	unsigned char buf[BUFSIZE];
-	prep_msg(buf,level,seed);
+	prep_initial_msg(buf,level,seed);
 
 	// query remote address info for UDP connection
 	struct addrinfo hints;
@@ -56,7 +57,7 @@ int main(int argc, char *argv[]) {
 	struct addrinfo *result;
 	int s = getaddrinfo(server_domain_name,port_in,&hints,&result);
 	if (s != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+		printf("getaddrinfo: %s\n", gai_strerror(s));
 		exit(EXIT_FAILURE);
 	}
 	// parse query response for remote address info
@@ -64,9 +65,6 @@ int main(int argc, char *argv[]) {
 	remote_addr = (struct sockaddr *)&remote_addr_ss;
 	memcpy(remote_addr, result->ai_addr, sizeof(struct sockaddr_storage));
 	parse_sockaddr(remote_addr, remote_ip, &remote_port);
-	if (verbose) {
-		fprintf(stderr, "Connecting to %s:%d\n", remote_ip, remote_port);
-	}
 	// initialize socket
 	sfd = socket(addr_fam, SOCK_DGRAM, 0);
 	if (sfd<0) {
@@ -78,6 +76,10 @@ int main(int argc, char *argv[]) {
 	local_addr = (struct sockaddr *)&local_addr_ss;
 	s = getsockname(sfd, local_addr, &addr_len);
 	parse_sockaddr(local_addr, local_ip, &local_port);
+	if (verbose) {
+		printf("Connecting to %s:%d\n", remote_ip, remote_port);
+		printf("Local port is %s:%d\n", local_ip, local_port);
+	}
 
 	// send message and collect response
 	unsigned char bufrecv[MAX_RECVSIZE];
@@ -91,7 +93,8 @@ int main(int argc, char *argv[]) {
 	unsigned int nonce, req;
 	chunk_len = bufrecv[0];
 	if (chunk_len>127) {
-		// TODO: server error
+		fprintf(stderr,"Server error 0x%x received\n", chunk_len);
+		exit(EXIT_FAILURE);
 	}
 	parse_response(bufrecv,chunk_len,treasure,&op,&op_param,&nonce);
 	if (verbose) {
@@ -103,29 +106,15 @@ int main(int argc, char *argv[]) {
 	// enter request + response loop
 	while (1) {
 		// adjust follow-up request based on opcode
-		switch(op) {
-		case 0:
-			req = (unsigned int)htonl(nonce+1);
-			break;
-		case 1:
-			remote_port = op_param;
-			populate_sockaddr(remote_addr,addr_fam,remote_ip,remote_port);
-			req = (unsigned int)htonl(nonce+1);
-			break;
-		case 2:
-			local_port = op_param;
-			populate_sockaddr(local_addr,addr_fam,local_ip,local_port);
-			req = (unsigned int)htonl(nonce+1);
-			break;
-		case 3:
-		default:
-			printf("Op-code %d not yet implemented. Exiting\n",op);
-			exit(EXIT_SUCCESS);
-		}
-
+		req = op_action(op,op_param,nonce);
 		if (verbose) {
+			printf("Connecting to %s:%d\n", remote_ip, remote_port);
+			printf("Local port is %s:%d\n", local_ip, local_port);
+			printf("Request is:");
 			print_bytes((unsigned char *)&req,sizeof(unsigned int));
 		}
+
+		// send follow-up request
 		request((unsigned char *)&req, sizeof(unsigned int), bufrecv);
 
 		// parse new response
@@ -133,15 +122,18 @@ int main(int argc, char *argv[]) {
 		if (chunk_len==0) { // we got all the treasure
 			break;
 		} else if (chunk_len>127) {
-			// TODO: server error
+			fprintf(stderr,"Server error 0x%x received\n", chunk_len);
+			exit(EXIT_FAILURE);
 		}
 		parse_response(bufrecv,chunk_len,treasure,&op,&op_param,&nonce);
 		if (verbose) {
 			printf("chunk length = %d\ttreasure = %s\top = 0x%x\top_param = 0x%x\tnonce = 0x%x\n",
 					chunk_len,treasure,op,op_param,nonce);
 		}
+		// advance treasure pointer for appending
 		treasure += chunk_len;
 	}
+	// null-terminate completed treasure and print
 	*treasure = '\0';
 	printf("%s\n",full_treasure);
 
@@ -149,7 +141,8 @@ int main(int argc, char *argv[]) {
 }
 
 
-void prep_msg(unsigned char* buf, int level, int seed) {
+// initial request holds several data items
+void prep_initial_msg(unsigned char* buf, int level, int seed) {
 	bzero(buf,BUFSIZE);
 
 	buf[1] = level;
@@ -161,6 +154,7 @@ void prep_msg(unsigned char* buf, int level, int seed) {
 	memcpy(&buf[6],&seed_ns,sizeof(unsigned short));
 }
 
+// send message stored in *buf to the server and collect the response into *bufrecv
 void request(unsigned char *buf, size_t send_size, unsigned char *bufrecv) {
 	ssize_t nwritten = sendto(sfd, buf, send_size, 0, remote_addr, addr_len);
 	if (nwritten<0) {
@@ -178,6 +172,7 @@ void request(unsigned char *buf, size_t send_size, unsigned char *bufrecv) {
 	}
 }
 
+// extract integer values of various sizes from *bufrecv
 void parse_response(unsigned char *bufrecv, unsigned char chunk_len, unsigned char *treasure, unsigned char *op, unsigned short *op_param, unsigned int *nonce) {
 	memcpy(treasure,&bufrecv[1],chunk_len);
 
@@ -192,6 +187,40 @@ void parse_response(unsigned char *bufrecv, unsigned char chunk_len, unsigned ch
 	*nonce = (unsigned int)ntohl(nonce_ns);
 }
 
+// adjust local/remote ports or collect extra context based on opcode
+unsigned int op_action(unsigned char op, unsigned short op_param, unsigned int nonce) {
+	switch((int)op) {
+	case 0:
+		return (unsigned int)htonl(nonce+1);
+	case 1:
+		// update remote port to match most recently received opcode parameter
+		remote_port = op_param;
+		populate_sockaddr(remote_addr,addr_fam,remote_ip,remote_port);
+		return (unsigned int)htonl(nonce+1);
+	case 2:
+		// update local port to match opcode parameter
+		local_port = op_param;
+		populate_sockaddr(local_addr,addr_fam,local_ip,local_port);
+		// close socket and reopen with new local port config
+		close(sfd);
+		sfd = socket(addr_fam, SOCK_DGRAM, 0);
+		if (sfd<0) {
+			perror("socket");
+			exit(EXIT_FAILURE);
+		}
+		if (bind(sfd, local_addr, sizeof(struct sockaddr_storage)) < 0) {
+			perror("bind()");
+			exit(EXIT_FAILURE);
+		}
+		return (unsigned int)htonl(nonce+1);
+	case 3:
+	default:
+		printf("Op-code %d not yet implemented. Exiting\n", (int)op);
+		exit(EXIT_SUCCESS);
+	}
+}
+
+// print contents of buffer with side-by-side hex/character display
 void print_bytes(unsigned char *bytes, int byteslen) {
 	int i, j, byteslen_adjusted;
 
